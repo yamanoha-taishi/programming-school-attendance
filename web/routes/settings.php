505f271d986752595dfa053da6fc1bb90dea8832
yamanoha <yamanoha.taishi.204ti@gmail.com>
2026-09-15 16:00:51 +0900

    fix: dashboard/settings系のauthミドルウェアをguardian/staffガード対応に修正

--- web/routes/settings.php
+++ web/routes/settings.php
@@ -7,14 +7,14 @@ use Illuminate\Auth\Middleware\RequirePassword;
 /* @end-chisel-password-confirmation */
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth'])->group(function () {
+Route::middleware(['auth:guardian,staff'])->group(function () {
     Route::redirect('settings', '/settings/profile');
 
     Route::get('settings/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('settings/profile', [ProfileController::class, 'update'])->name('profile.update');
 });
 
-Route::middleware(['auth', 'verified'])->group(function () {
+Route::middleware(['auth:guardian,staff', 'verified'])->group(function () {
     Route::delete('settings/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::get('settings/security', [SecurityController::class, 'edit'])
