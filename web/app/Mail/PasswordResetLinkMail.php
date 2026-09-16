6f080b8dd6266037a16fa651487b281e04e9c81b
yamanoha <yamanoha.taishi.204ti@gmail.com>
2026-09-16 20:35:32 +0900

    fix: パスワード再設定メールをキュー経由の非同期送信に変更し応答時間差による列挙を防止

--- web/app/Mail/PasswordResetLinkMail.php
+++ web/app/Mail/PasswordResetLinkMail.php
@@ -3,13 +3,14 @@
 namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Attachment;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class PasswordResetLinkMail extends Mailable
+class PasswordResetLinkMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
