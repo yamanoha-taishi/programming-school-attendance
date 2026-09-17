920b31f0a3424fad93c727361731c335ebc0dcba
yamanoha <yamanoha.taishi.204ti@gmail.com>
2026-09-18 01:10:21 +0900

    fix: 実質機能していないverifiedミドルウェアをguardian/staffのルートから削除

--- web/routes/settings.php
+++ web/routes/settings.php
@@ -14,7 +14,7 @@ Route::middleware(['auth:guardian,staff'])->group(function () {
     Route::patch('settings/profile', [ProfileController::class, 'update'])->name('profile.update');
 });
 
-Route::middleware(['auth:guardian,staff', 'verified'])->group(function () {
+Route::middleware(['auth:guardian,staff'])->group(function () {
     Route::delete('settings/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::get('settings/security', [SecurityController::class, 'edit'])
--- web/routes/web.php
+++ web/routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'welcome')->name('home');
 
-Route::middleware(['auth:guardian,staff', 'verified'])->group(function () {
+Route::middleware(['auth:guardian,staff'])->group(function () {
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
 });
 
