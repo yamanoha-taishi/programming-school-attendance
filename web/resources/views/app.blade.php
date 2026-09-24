1392252321fb2f1a6c007cd0ff8abaf4a25b7700
yamanoha <yamanoha.taishi.204ti@gmail.com>
2026-09-24 20:41:04 +0900

    chore: 未使用になったInstrument Sansの読み込みを削除

--- web/resources/views/app.blade.php
+++ web/resources/views/app.blade.php
@@ -34,8 +34,6 @@
         <link rel="icon" href="/favicon.svg" type="image/svg+xml">
         <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
-        @fonts
-
         @viteReactRefresh
         @vite(['resources/css/app.css', 'resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
         <x-inertia::head>
